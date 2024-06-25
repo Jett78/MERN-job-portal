90374a0d9249870f9e61c30a801e9a1b6fc4d32b
Jeet <jetdeula@gmail.com>
2024-06-25 21:07:07 +0545

    react-router routes added

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <App/>,
+    children:[
+        {
+            path:"/",
+            element:<Home/>,
+        },
+        {
+            path:"/about",
+            element:<About/>,
+        },
+    ]
   },
 ]);
 
