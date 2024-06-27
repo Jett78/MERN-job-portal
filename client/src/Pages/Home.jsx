b70e5a4cafa1fbd43e805957ba1d1d8aca96bf6d
Jeet <jetdeula@gmail.com>
2024-06-27 21:30:59 +0545

    pagination added

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,14 +2,41 @@ import React, { useEffect, useState } from 'react'
 import SearchJob from '../components/SearchJob'
 import Card from '../components/Card';
 import Jobs from '../components/Jobs';
+import Sidebar from '../sidebar/Sidebar';
 
 const Home = () => {
   const[selectedCategory,setSelectedCategory] = useState();
   const[jobs,setJobs] = useState([]);
+  const[isLoading,setLoading] = useState(false);
+  const[currentPage,setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
+    setLoading(true)
     fetch("jobs.json").then(res => res.json()).then(data => {setJobs(data)})
+    setLoading(false);
   },[])
+
+  //pagination function--calculating index range
+  const calculatePageRange = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return {startIndex,endIndex}; 
+  }
+
+  //function for next page
+  const nextPage = () => {
+    if(currentPage < Math.ceil(filteredItems.length/itemsPerPage)){
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  //function for previous page
+  const prevPage = () => {
+    if(currentPage > 1){
+      setCurrentPage(currentPage - 1);
+    }
+  }
 
   //handle input change
   const [query,setQuery] = useState("");
@@ -20,7 +47,6 @@ const Home = () => {
 
   //filter jobs by title
   const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-  console.log(filteredItems)
 
   //radio-filtering
   const handleChange = (event) => {
@@ -32,7 +58,7 @@ const Home = () => {
     setSelectedCategory(event.target.value)
   }
 
-  //main function
+  //---main function----
   const filteredData = (jobs,selected,query) => {
     let filteredJobs = jobs;
 
@@ -51,10 +77,14 @@ const Home = () => {
       ));
       console.log(filteredJobs)
     }
+    const {startIndex,endIndex} = calculatePageRange();    //slice data based on current page for pagination
+    filteredJobs = filteredJobs.slice(startIndex,endIndex)
     return filteredJobs.map((data,i) => <Card key={i} data={data}/>)
   }
 
   const result = filteredData(jobs,selectedCategory,query);
+
+  
 
   return (
    <div>
@@ -62,9 +92,32 @@ const Home = () => {
 
     {/* main content */}
     <section className='bg-zinc-100 grid grid-cols-4 justify-center gap-4 p-10'>
-     <div className='bg-white'>left</div>
-     <div className='bg-white col-span-2'> <Jobs result={result}/></div>
-     <div className='bg-white'>right</div>
+
+      {/* left-side */}
+     <div className='bg-white p-4'><Sidebar handleChange={handleChange} handleClick={handleClick}/></div>
+
+     <div className='bg-white col-span-2'> 
+      {
+        isLoading ? (<p>Loading....</p>) : result.length > 0 ? (<Jobs result={result}/>) : 
+        <div>
+          <h3 className="font-semibold text-center text-xl px-4 pt-2">No Jobs Found</h3>
+          <img src="./notfound.png" alt="" />
+        </div>
+       }
+
+      {/*  pagination */}
+      {result.length > 0 ? (
+        <div>
+          <button onClick={prevPage} >Previous</button>
+          <span>  Page {currentPage} of {Math.ceil(filteredItems.length/itemsPerPage)}</span>
+          <button onClick={nextPage} disabled={currentPage === Math.ceil(filteredItems.length/itemsPerPage)}>Next</button>
+        </div>):""
+      }
+
+     </div>
+
+     {/* right-side */}
+     <div className='bg-white p-4'>right</div>
     </section>
    </div>
   )
